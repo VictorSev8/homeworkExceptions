61195e0aa4a16436d65a2ac6d2d424b41ece9f0e
Victor Severin <severinv@list.ru>
2023-03-26 18:18:53 +0300

    initial commit

---
+++ src/Authenticator.java
@@ -0,0 +1,39 @@
+public class Authenticator {
+    private String login;
+
+    private String password;
+
+    private String confirmPassword;
+
+    public Authenticator(String login, String password, String confirmPassword) {
+        this.login = login;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+
+}
